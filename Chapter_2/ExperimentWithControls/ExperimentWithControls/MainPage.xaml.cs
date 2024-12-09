867ba1d123094ef8a113f10dc85e63c68185ad26
Evan <md.mofazzalhossain18@gmail.com>
2024-12-09 19:39:09 +0600

    "BirdsPicker"complete

---
+++ Chapter_2/ExperimentWithControls/ExperimentWithControls/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace ExperimentWithControls
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+           InitializeComponent();
+
+            BirdPicker.ItemsSource = new String[] {
+                "Duck",
+                "Pigeon",
+                "Penguin",
+                "Ostrich",
+                "Owl"
+
+            };
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void Entry_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+        {
+            EnteredText.Text = e.NewTextValue;
+        }
+
+        private void Slider_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            SliderValue.Text = e.NewValue.ToString();   
+        }
+
+        private void Stepper_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            StepperValue.Text = e.NewValue.ToString();
+
+        }
+
+        private void AddBird_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Birds.Text = Birds.Text + Environment.NewLine + BirdPicker.SelectedItem;
+        }
+    }
+
+}
